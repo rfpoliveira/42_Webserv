fc017463cbd95fecf0542e9276088ae74280c602
Matheus Diniz <mreinald@student.42porto.com>
2026-08-14 01:06:48 +0100

    chore: new RequestHandler name

--- includes/server/RequestHandler.hpp
+++ includes/server/RequestHandler.hpp
@@ -1,6 +1,15 @@
 #pragma once
 
-#include "Client.hpp"
-#include "../config/Config.hpp"
+#include <Client.hpp>
+#include <Config.hpp>
+#include <Location.hpp>
 
-std::string requestHandler(const Client &client, const Config &config);
+class RequestHandler
+{
+	private:
+		static std::string handleGet(const Request &request, const Location &location);
+		static std::string handlePost(const Request &request, const Location &location);
+		static std::string handleDelete(const Request &request, const Location &location);
+	public:
+		static std::string handler(const Client &client, const Config &config);
+};
